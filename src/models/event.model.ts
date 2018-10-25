a4e53e3d8ab3542e1de1204272e148de394e2bf4
Timothy Bott <timothy.bott87@gmail.com>
2018-10-25 10:53:23 +0200

    error handling

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -39,7 +39,7 @@ export class EventModel {
 	}
 
 	protected validate() {
-		if (!this.raw.name) return this.addError('MISSING_TYPE');
+		if (!this.raw.name) return this.addError('MISSING_EVENT_NAME', 'name:string');
 		if (!this.eventConfig) return this.addError('EVENT_DOES_NOT_EXIST');
 		if (!this.raw.params && this.eventConfig.requiredParams.length !== 0) return this.addError("EVENT_REQUIRES_PARAMETERS");
 		// validate the parameters according to config
