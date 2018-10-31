f29e11a192e4f7b2c4306d59e834cdfc02cf37b0
Timothy Bott <timothy.bott87@gmail.com>
2018-10-31 09:53:53 +0100

    x

--- src/interfaces/server-to-client-event.interface.ts
+++ src/interfaces/server-to-client-event.interface.ts
@@ -1,5 +1,5 @@
 export interface ServerToClientEventInterface {
-	name: 'READY' | 'HANGUP' | 'CLIENT_SIP_RINGING' | 'REMOTE_RINGING' | 'CALL_CONNECTED';
+	name: 'READY' | 'HANGUP' | 'CLIENT_SIP_RINGING' | 'REMOTE_RINGING' | 'CALL_CONNECTED' | 'ERROR';
 	params?: Array<{
 		key: string,
 		value: string
