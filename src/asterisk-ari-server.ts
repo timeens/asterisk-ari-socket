6f2312a71a6ce953a180381d9a222cffad30e008
Timothy Bott <timothy.bott87@gmail.com>
2018-12-04 14:31:01 +0100

    tls support added

--- src/asterisk-ari-server.ts
+++ src/asterisk-ari-server.ts
@@ -2,6 +2,8 @@ import * as ws from 'ws';
 import { WebsocketClient } from './websocket/websocket-client';
 import { AppLogger } from './logger/app-logger';
 import { InboundStasisWebsocket } from './websocket/inbound-stasis-websocket';
+import * as fs from 'fs';
+import * as https from 'https';
 
 export class AsteriskAriServer {
 
@@ -10,14 +12,29 @@ export class AsteriskAriServer {
 	protected port: any;
 
 	constructor() {
-		let WebSocketServer = ws.Server;
-		this.port = process.env['SERVER_PORT'] || 3001;
-		this.wsServer = new WebSocketServer({port: this.port});
+		this.createServer();
 		this.initInboundSocket();
 	}
 
+	private createServer() {
+		let WebSocketServer = ws.Server;
+		if (process.env['SSL_KEY_PATH'] || process.env['SSL_CERTIFICATE_PATH']) {
+			let securePort = 8443;
+			const privateKey = fs.readFileSync(process.env['SSL_KEY_PATH'], 'utf8');
+			const certificate = fs.readFileSync(process.env['SSL_CERTIFICATE_PATH'], 'utf8');
+			const credentials = {key: privateKey, cert: certificate};
+			const httpsServer = https.createServer(credentials);
+			httpsServer.listen(securePort);
+			this.wsServer = new WebSocketServer({server: httpsServer});
+			AppLogger.info(`Server listening on port ${securePort} (TLS protected)`);
+		} else {
+			this.port = process.env['SERVER_PORT'] || 3001;
+			this.wsServer = new WebSocketServer({port: this.port});
+			AppLogger.info(`Server listening on port ${this.port} (unsecure connection)`);
+		}
+	}
+
 	listen() {
-		AppLogger.info(`Server listening on port ${this.port}`);
 		this.wsServer.on('connection', client => {
 			AppLogger.info(`New client connected`);
 			new WebsocketClient(client);
