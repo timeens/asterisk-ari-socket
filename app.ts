8f8e73a05bc531e465a9b12ef49c17280c556fe9
Timothy Bott <timothy.bott87@gmail.com>
2018-12-11 11:22:45 +0100

    keep alive added

--- app.ts
+++ app.ts
@@ -7,18 +7,19 @@ import { AriRest } from './src/asterisk-rest/ari-rest';
 dotenv.config();
 
 // init Server
-try {
-	AppLogger.info('Starting up server...');
-	let rest = new AriRest();
-	rest.restAsterisk.isAvailable()
-		.then((asteriskVersion) => {
-			AppLogger.info(`Asterisk ready: Version: ${asteriskVersion}`);
-			let server = new AsteriskAriServer();
-			server.listen();
-		})
-		.catch(err => {
-			throw Error(err);
-		})
-} catch (err) {
-	console.log("Error", err);
-}
+AppLogger.info('Starting up server...');
+let rest = new AriRest();
+rest.restAsterisk.isAvailable()
+	.then((asteriskVersion) => {
+		AppLogger.info(`Asterisk ready: Version: ${asteriskVersion}`);
+		let server = new AsteriskAriServer();
+		server.listen();
+	})
+	.catch(err => {
+		throw Error(err);
+	});
+
+// keep alive node process
+setInterval(() => {
+	AppLogger.debug('keep alive');
+}, 1000);
